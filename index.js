94323cd1766e1c0645bfe1cf914e062a6f501e34
Manuel Escala <>
2021-04-24 03:48:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let adv_contra = 'Rocket'
+let contra = '';
+console.log('Escriba la contraseña')
+
+while (adv_contra != contra){
+  contra= prompt('Cual es la contrasena que estoy usando')
+}
+
+console.log('La contraseña es correcta')
+
